5fcc1326cf3ec1751582fd2c00e4ca33d4e8d9f1
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2025-03-15 21:21:22 +0545

    handle gesture for unlock or redirect to DeliveryLogin like up up down left right swap method created

--- src/features/auth/CustomerLogin.tsx
+++ src/features/auth/CustomerLogin.tsx
@@ -1,18 +1,59 @@
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView, Animated } from 'react-native'
+import React, { useState } from 'react'
 import {GestureHandlerRootView,PanGestureHandler,State} from 'react-native-gesture-handler'
 import CustomSafeAreaView from '@components/global/CustomSafeAreaView'
 import ProductSlider from '@components/login/ProductSlider'
+import CustomText from '@components/ui/CustomText'
+import { RFValue } from 'react-native-responsive-fontsize'
+import { resetAndNavigate } from '@utils/NavigationUtils'
+import { Colors } from '@utils/Constants'
+
 
 const CustomerLogin = () => {
+
+  const [gestureSequence,setGestureSequence] = useState<string[]>([])
+
+  const handleGesture = ({nativeEvent}:any) =>{
+    if(nativeEvent.state === State.END){
+      const {translationX,translationY} = nativeEvent
+      let direction =''
+      if(Math.abs(translationX) > Math.abs(translationY)){
+        direction = translationX > 0 ? "right":"left"
+      } else{
+        direction = translationY > 0 ? "down":"up"
+      }
+      const newSequence = [...gestureSequence,direction].slice(-5)
+      setGestureSequence(newSequence)
+      if(newSequence?.join(' ')=== 'up up down left right'){
+        setGestureSequence([ ])
+        resetAndNavigate("DeliveryLogin")
+      }
+
+
+    }
+  }
+
   return (
     <GestureHandlerRootView style={styles.container}>
      <View style={styles.container}>
         <CustomSafeAreaView>
             <ProductSlider/>
+            <PanGestureHandler onHandlerStateChange={handleGesture}>
+              <Animated.ScrollView bounces={false}
+              keyboardDismissMode="on-drag"
+              keyboardShouldPersistTaps="handled"
+              contentContainerStyle={styles.subContainer}
+              >
+
+              </Animated.ScrollView>
+            </PanGestureHandler>
         </CustomSafeAreaView>
 
 <View style={styles.footer}>
+  <SafeAreaView/>
+  <CustomText fontSize={RFValue(9)}>
+    By Continuing, you agree to our Terms of Service & Privacy Policy.
+  </CustomText>
   <SafeAreaView/>
 </View>
 
@@ -26,8 +67,24 @@ const styles = StyleSheet.create({
     container:{
         flex:1
     },
+    subContainer:{
+      flex:1,
+      justifyContent:'flex-end',
+      alignItems:"center",
+      marginBottom:20
+    },
     footer:{
-
+      borderTopWidth:0.8,
+      borderColor:Colors.border,
+      paddingBottom:10,
+      zIndex:22,
+      position:'absolute',
+      bottom:0,
+      justifyContent:'center',
+      alignItems:"center",
+      padding:10,
+      backgroundColor:"#f8f9fc",
+      width:"100%"
     }
     
 })
